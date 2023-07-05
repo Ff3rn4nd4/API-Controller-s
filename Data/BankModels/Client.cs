89025e20bf01bc7dd89ff29386c6d4b2fce5710a
Ff3rn4nd4 <guerrareyes_fernanda@hotmail.com>
2023-07-05 01:45:27 -0600

    + validaciones por modelo json ignore

--- Data/BankModels/Client.cs
+++ Data/BankModels/Client.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace BankAPI.Data.BankModels;
 
 public partial class Client
 {
+    public Client()
+    {
+        Accounts = new HashSet<Account>();
+    }
     public int Id { get; set; }
     [MaxLength(200, ErrorMessage = "El nombre no debe de exceder los 200 caracteres")]
     public string Name { get; set; } = null!;
@@ -18,5 +23,6 @@ public partial class Client
     
     public DateTime? RegDate { get; set; }
 
+    [JsonIgnore]
     public virtual ICollection<Account> Accounts { get; set; } = new List<Account>();
 }
