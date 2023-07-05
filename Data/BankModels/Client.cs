4d9c4a05181b42cc485d091a75879c7b5452a69e
Ff3rn4nd4 <guerrareyes_fernanda@hotmail.com>
2023-07-05 01:40:57 -0600

    datanotations

--- Data/BankModels/Client.cs
+++ Data/BankModels/Client.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BankAPI.Data.BankModels;
 
 public partial class Client
 {
     public int Id { get; set; }
-
+    [MaxLength(200, ErrorMessage = "El nombre no debe de exceder los 200 caracteres")]
     public string Name { get; set; } = null!;
-
+    //DATA ANOTATIOIN: atributos para propiedades en los modelos
+    [MaxLength(40, ErrorMessage = "El numero de telefono no debe de exceder los 40 digitos")]
     public string PhoneNumber { get; set; } = null!;
-
+    [MaxLength(50, ErrorMessage = "El email no debe de exceder los 50 caracteres")]
     public string Email { get; set; }
-
+    
     public DateTime? RegDate { get; set; }
 
     public virtual ICollection<Account> Accounts { get; set; } = new List<Account>();
